bd1993ff49a5137f9d3bc3cd928a89ee891e30d8
Mariam Mazen <mariamMazen>
2021-04-22 02:57:23 +0200

    project4

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="mariammazen/api"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment project4-submission --image=mariammazen/api:firsttry --port=80 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward project4-submission-55df679b55-lb7wc 8000:80
 
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=mariammazen/api
 
 # Step 2:  
 # Authenticate & tag
+docker login --username=mariammazen
 echo "Docker ID and Image: $dockerpath"
+docker tag deec8cab2254 mariammazen/api:firsttry
 
 # Step 3:
 # Push image to a docker repository
+docker push mariammazen/api:firsttry
